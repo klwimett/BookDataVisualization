002fe937e6dbc66efcf08cd240e06c831be44411
Dirk van Oosterbosch <dirk@innovactory.com>
2017-05-21 20:06:15 +0200

    just_words.py is now counting word frequencies

--- just_words.py
+++ just_words.py
@@ -5,9 +5,15 @@ with open('just_titles_short.json') as file1:
 
 titles_split = []
 
-for t in titles:
-    t = str(t)
-    x = str.split(t)
-    titles_split.append(x)
+def wordListToFreqDict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    return dict(zip(wordlist,wordfreq))
 
-print titles_split
+for t in titles:
+    t = str(t).strip()
+    words_per_title = t.split(" ")
+    titles_split += words_per_title
+
+print "The unique words used in the titles, and their frequeny"
+print wordListToFreqDict(titles_split)
+
